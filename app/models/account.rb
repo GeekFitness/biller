8de060c9aed056c12cf932d4996d15cf00a3956b
BJ Keeton <bjk@geekfitness.net>
2016-07-16 19:30:07 -0500

    add basic validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,8 @@
 class Account < ApplicationRecord
 	has_many :account_entries
+
+	validates :name, presence: true, 
+							length: {in: 3..20,
+											message: "Use a good name, bro"},
+							uniqueness: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
 	belongs_to :customer
 	has_and_belongs_to_many :employees, :join_table => "employees_projects"
+
+	validates :name, presence: true, 
+						length: {in: 3..20}, 
+						uniqueness: true
+
+
 end
